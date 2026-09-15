59aa54f8f316e41de3009a89de2af088f13d55fa
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2026-09-14 20:31:41 -0500

    Se crea ControllerSpec

---
+++ src/test/java/com/danielesteban/music_app/controller/ControllerSpec.java
@@ -0,0 +1,7 @@
+package com.danielesteban.music_app.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(value = AlbumController.class)
+public class ControllerSpec {
+}
